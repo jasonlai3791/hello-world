df76b5aba0edf5ed48522d9a80afd3588199e2b1
Jason Lai <jason.lai@quantatw.com>
2020-03-10 16:53:02 +0800

    Add Monster 1

--- hello.c
+++ hello.c
@@ -12,4 +12,5 @@ main()
     printf("Hello World, this is Jason Lai\n");
     printf("Hello World, this is Dog 1\n");
     printf("Hello World, this is Dog 2\n");
+    printf("Hello World, this is Monster 1\n");
 }
