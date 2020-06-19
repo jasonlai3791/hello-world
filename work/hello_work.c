90e23b52b200185431e268fcde452843747b8b8f
Jason Lai <jason.lai@quantatw.com>
2020-06-19 18:09:42 +0800

    Create work folder and add hello_work.c in it

---
+++ work/hello_work.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+main()
+{
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 9; i++)
+        j++;
+    for (i = 0; i < 9; i++)
+        j = j + i;
+
+
+    while (i < 9)
+    {
+        i++
+    }
+    while (j < 9)
+    {
+        j++
+    }
+    printf("Hello World, this is Jason Lai\n");
+    printf("Hello World, this is Dog -1\n");
+    printf("Hello World, this is Dog 0\n");
+    printf("Hello World, this is Dog 1\n");
+    printf("Hello World, this is Dog 2\n");
+    printf("Hello World, this is Dog 3\n");
+    printf("Hello World, this is Monster 1\n");
+    printf("Hello World, this is Monster 2\n");
+    printf("Hello World, this is Fish 1\n");
+    printf("Hello World, this is Fish 2\n");
+    printf("Hello World, this is Fetch 1\n");
+    while (i < 10)
+    {
+        i++;
+    }
+    printf("Hello World, this is Fetch 2\n");
+    while (j < 10)
+    {
+        j++;
+    }
+}
